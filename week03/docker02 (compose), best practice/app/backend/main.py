8b569891448f3f8c088c2cde25890da304a63c6c
accxracy <gkola22907@gmail.com>
2025-08-16 16:05:52 +0000

    Update main.py

--- week03/docker02 (compose), best practice/app/backend/main.py
+++ week03/docker02 (compose), best practice/app/backend/main.py
@@ -1,1 +1,42 @@
+from flask import Flask
+import psycopg2
+
+
+app = Flask(__name__)
+
+def get_db_con():
+    connection = psycopg2.connect(
+        host='database',
+        dbname='flask_db',
+        user='neo',
+        password='1111',
+        port='5432'
+    )
+    return connection
+
+
+@app.route('/')
+def index():
+    conn = get_db_con()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users;")
+    users = cur.fetchall()
+    cur.close()
+    conn.close()
+    return f"{' '.join([user[1] for user in users])}"
+
+
+@app.route('/add/<user>')
+def add(user):
+    conn = get_db_con()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO users (name) VALUES (%s)", (user, ))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return f"{user} Добавлен"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
